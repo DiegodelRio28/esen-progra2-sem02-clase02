2bab7a076b1b1906a5a5b91e82943e7094f9ef5e
baylagas <balbino_a@hotmail.com>
2021-01-29 20:12:45 -0600

    finished script code for todo list

--- script.js
+++ script.js
@@ -9,10 +9,44 @@ for(var i=0; i<myNodelist.length; i++){
 }
 
 //dar click en close y ocultar el elemento presionado
-var close = document.getElementsByClassName("close");
-for(var i=0; i<close.length; i++){
-    close[i].onclick = function(){
-        var div = this.parentElement;
-        div.style.display="none";
-    };
+function addCloseFunction(){
+    var close = document.getElementsByClassName("close");
+    for(var i=0; i<close.length; i++){
+        close[i].onclick = function(){
+            var div = this.parentElement;
+            div.style.display="none";
+        };
+    }
+}
+
+addCloseFunction();
+
+//como marcar un elemento de la lista como checked
+var list = document.querySelector("ul");
+list.addEventListener('click', function(ev){
+    if(ev.target.tagName === 'LI'){
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+//crear un nuevo elemento de la lista al dar click en add
+function newElement(){
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if(inputValue===""){
+        alert("debe escribir una tarea!");
+    }else{
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value="";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00d7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    addCloseFunction();
 }
